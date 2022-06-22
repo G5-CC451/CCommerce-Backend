2d271012d4766e6cd4a11ff4410c831e39fe733c
Jhonny Lorenzo <jhonny.lorenzo.r@uni.pe>
2022-06-22 15:44:46 -0500

    fix: connection db from cloud mongo atlas

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ require("dotenv").config();
 const app = express();
 
 // db
+const db = String(process.env.MONGO_DB_URI);
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(db)
   .then(() => console.log("CCOMMERCE DB: CONECTADO"))
   .catch((err) => console.log("CCOMMERCE DB: ERROR DE CONEXIÓN", err));
 
 // middlewares
 app.use(morgan("dev"));
-app.use(express.json({limit: "2mb"}));
+app.use(express.json({ limit: "2mb" }));
 app.use(cors());
 
 // routes middleware
@@ -27,4 +28,6 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 // port
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Servidor esta corriendo en el puerto ${port}`));
+app.listen(port, () =>
+  console.log(`Servidor esta corriendo en el puerto ${port}`)
+);
