a4f84dc2e0d8560feb776da859db124f439872fa
Jhonny Lorenzo <jhonny.lorenzo.r@uni.pe>
2022-07-18 00:02:58 -0500

    fix: lightweight searchFilters services

--- controllers/product.js
+++ controllers/product.js
@@ -150,25 +150,6 @@ const handleQuery = async (req, res, query) => {
   res.json(products);
 };
 
-const handlePrice = async (req, res, price) => {
-  try {
-    let products = await Product.find({
-      price: {
-        $gte: price[0],
-        $lte: price[1],
-      },
-    })
-      .populate("category", "_id name")
-      .populate("subs", "_id name")
-      .populate("postedBy", "_id name")
-      .exec();
-
-    res.json(products);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const handleCategory = async (req, res, category) => {
   try {
     let products = await Product.find({ category })
@@ -179,62 +160,6 @@ const handleCategory = async (req, res, category) => {
   } catch (err) {
     console.log(err);
   }
-};
-
-const handleStar = (req, res, stars) => {
-  Product.aggregate([
-    {
-      $project: {
-        document: "$$ROOT",
-        floorAverage: {
-          $floor: { $avg: "$ratings.star" },
-        },
-      },
-    },
-    { $match: { floorAverage: stars } },
-  ])
-    .limit(12)
-    .exec((err, aggregates) => {
-      if (err) console.log("AGGREGATE ERROR", err);
-      Product.find({ _id: aggregates })
-        .populate("category", "_id name")
-        .populate("subs", "_id name")
-        .populate("postedBy", "_id name")
-        .exec((err, products) => {
-          if (err) console.log("PRODUCT AGGREGATE ERROR", err);
-          res.json(products);
-        });
-    });
-};
-
-const handleSub = async (req, res, sub) => {
-  const products = await Product.find({ subs: sub })
-    .populate("category", "_id name")
-    .populate("subs", "_id name")
-    .populate("postedBy", "_id name")
-    .exec();
-
-  res.json(products);
-};
-
-const handleShipping = async (req, res, shipping) => {
-  const products = await Product.find({ shipping })
-    .populate("category", "_id name")
-    .populate("subs", "_id name")
-    .populate("postedBy", "_id name")
-    .exec();
-
-  res.json(products);
-};
-
-const handleColor = async (req, res, color) => {
-  const products = await Product.find({ color })
-    .populate("category", "_id name")
-    .populate("subs", "_id name")
-    .populate("postedBy", "_id name")
-    .exec();
-
-  res.json(products);
 };
 
 const handleBrand = async (req, res, brand) => {
@@ -248,42 +173,16 @@ const handleBrand = async (req, res, brand) => {
 };
 
 exports.searchFilters = async (req, res) => {
-  const { query, price, category, stars, sub, shipping, color, brand } =
-    req.body;
+  const { query, category, brand } = req.body;
 
   if (query) {
     console.log("query --->", query);
     await handleQuery(req, res, query);
   }
 
-  if (price !== undefined) {
-    console.log("price ---> ", price);
-    await handlePrice(req, res, price);
-  }
-
   if (category) {
     console.log("category ---> ", category);
     await handleCategory(req, res, category);
-  }
-
-  if (stars) {
-    console.log("stars ---> ", stars);
-    await handleStar(req, res, stars);
-  }
-
-  if (sub) {
-    console.log("sub ---> ", sub);
-    await handleSub(req, res, sub);
-  }
-
-  if (shipping) {
-    console.log("shipping ---> ", shipping);
-    await handleShipping(req, res, shipping);
-  }
-
-  if (color) {
-    console.log("color ---> ", color);
-    await handleColor(req, res, color);
   }
 
   if (brand) {
